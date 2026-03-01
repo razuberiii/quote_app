09ee79e759bba3e666a801fdd59ba94ff282c144
razuberiii <razuberi@163.com>
2026-03-01 23:35:15 +0800

    seo

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -5,10 +5,10 @@ class SitemapsController < ApplicationController
   def show
     @sitemap_urls = [
       { loc: root_url, changefreq: "daily", priority: "1.0" },
+      { loc: foreign_trade_quotation_software_url, changefreq: "weekly", priority: "0.9" },
+      { loc: quotation_crm_for_export_teams_url, changefreq: "weekly", priority: "0.9" },
       { loc: demo_url, changefreq: "weekly", priority: "0.8" },
-      { loc: sample_quote_url, changefreq: "weekly", priority: "0.7" },
-      { loc: new_user_registration_url, changefreq: "weekly", priority: "0.6" },
-      { loc: new_user_session_url, changefreq: "monthly", priority: "0.4" }
+      { loc: sample_quote_url, changefreq: "weekly", priority: "0.7" }
     ]
     @lastmod = Date.current.iso8601
   end
